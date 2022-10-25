ecba6518a91e180dc8c757acfe5a0e0eab5cde9a
JTraglia1 <jt9469@desales.edu>
2022-10-24 23:46:02 -0400

    Final with EncryptFragment

--- app/src/main/java/com/hfad/secretmessage/MessageFragment.java
+++ app/src/main/java/com/hfad/secretmessage/MessageFragment.java
@@ -2,16 +2,36 @@ package com.hfad.secretmessage;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MessageFragment extends Fragment {
-
+public class MessageFragment extends Fragment
+{
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_message, container, false);
+        Button nextButton = view.findViewById(R.id.btn_next);
+        TextView messageView = view.findViewById(R.id.etv_message);
+
+        nextButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                String msg = messageView.getText().toString();
+
+                MessageFragmentDirections.ActionMessageFragmentToEncryptFragment action =
+                        MessageFragmentDirections.actionMessageFragmentToEncryptFragment(msg);
+
+                Navigation.findNavController(view).navigate(action);
+            }
+        });
+        return view;
     }
+
 }
